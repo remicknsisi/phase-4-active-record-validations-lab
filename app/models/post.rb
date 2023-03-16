d8a0b773a75a2aa23bd05870438958610cda5240
Sienna <remicknsisi@gmail.com>
2023-03-16 10:51:03 -0400

    commit tree b1a3a7b3f9ace5b251927f156e462f025babdf1e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :title_cannot_be_clickbait
+
+    def title_cannot_be_clickbait
+        if title == "True Facts"
+            errors.add(:title, "Invalid title!")
+        end
+    end
 end
